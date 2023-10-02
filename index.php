6998bebd42c9e67d2fd83197931b235f5374918c
pradip-1137 <96612151+pradip-1137@users.noreply.github.com>
2023-10-02 22:01:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 <h1> !...Welcome to OPENSHIFT with WEBHOOK...!</h1>
 
 <?php
-echo "This is S2I example version-1 "
+echo "Code updated successfully....! "
+echo "This is S2I example version-2 "
 ?>
 
 </body>
